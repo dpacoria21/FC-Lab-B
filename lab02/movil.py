193e753125589ebcee9a825b7e52514d881dbd63
Your Name <dpacoria@unsa.edu.pe>
2024-05-06 23:08:21 -0500

    Se hizo el ambos programas iterativos para el lab02

--- lab02/movil.py
+++ lab02/movil.py
@@ -1,40 +1,7 @@
-# import math
-
-# def isPositive(x):
-#     if x>=0:
-#         return True
-#     else:
-#         return False
-
-# def isGreaterThanZero(x):
-#     if x>0:
-#         return True
-#     else:
-#         return False
-
-# def isPossibleToGet(b, a, c):
-#     if (b*b) - (4*a*c) < 0 :
-#         return [False, -1]
-#     else:
-#         return [True, (-b+math.sqrt(b*b - 4*a*c))/(2*a)]
-
-# isContinue = True
-# while(isContinue):
-
-#     # distancia no es necesaria para resolver este problema
-#     m = float(input("Ingrese la masa del movil (en kg): "))
-#     t = float(input("Ingrese el tiempo del movimiento (en s): "))
-#     vi = float(input("Ingrese la velocidad inicial (en m/s): "))
-#     vf = float(input("Ingrese la velocidad final (en m/s): "))
-
-#     value = input(f"Desea obtener otro valor? (Yes=y) (No=n): ")
-#     if value=="n" or value=="No" or value=="no" or value=="NO":
-#         print("Gracias por usar el programa!!!")
-#         isContinue = False
-
 import matplotlib.pyplot as plt
 
 def calcular_fuerza(m, vi, vf, t):
+    # formula para hallar la aceleracion con la vi, vf y t
     return m * (vf - vi) / t
 
 def graficar_proceso(t, fuerza):
@@ -47,19 +14,27 @@ def graficar_proceso(t, fuerza):
 
 def main():
 
-    #Distancia no es necesaria para este calculo
-    m = float(input("Ingrese la masa del móvil (kg): "))
-    vi = float(input("Ingrese la velocidad inicial (m/s): "))
-    vf = float(input("Ingrese la velocidad final (m/s): "))
-    t = float(input("Ingrese el tiempo tomado (s): "))
-    
-    fuerza = calcular_fuerza(m, vi, vf, t)
-    print("La fuerza que experimenta el móvil es:", fuerza, "N")
-    
-    # Crear un arreglo de tiempo para graficar
-    tiempo_grafica = [0, t]
-    fuerza_grafica = [0, fuerza]
-    
-    graficar_proceso(tiempo_grafica, fuerza_grafica)
+    isContinue = True
+    while(isContinue):
+
+        #Distancia no es necesaria para este calculo
+        m = float(input("Ingrese la masa del móvil (kg): "))
+        vi = float(input("Ingrese la velocidad inicial (m/s): "))
+        vf = float(input("Ingrese la velocidad final (m/s): "))
+        t = float(input("Ingrese el tiempo tomado (s): "))
+        
+        f = calcular_fuerza(m, vi, vf, t)
+        print(f"La fuerza que experimenta el móvil es: {f} N")
+        
+        # Crear un arreglo de tiempo para graficar
+        t_x = [0, t]
+        f_y = [0, f]
+        
+        graficar_proceso(t_x, f_y)
+
+        value = input(f"¿Desea usar otra vez el programa? (Yes=y) (No=n): ")
+        if value=="n" or value=="No" or value=="no" or value=="NO":
+            print("Gracias por usar el programa!!!")
+            isContinue = False
 
 main()
